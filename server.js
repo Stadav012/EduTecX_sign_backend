6ab220445d72b88641bd5ec20a54ee46a31c4b86
Stadav01 <stanley.ndlovu@ashesi.edu.gh>
2025-01-07 14:43:36 +0000

    changed vercel

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(bodyParser.json());
@@ -18,7 +17,7 @@ app.use(cors());
 // Or configure CORS to allow specific origin
 app.use(
     cors({
-        origin: 'http://localhost:5173', // Your frontend URL
+        origin: 'https://edutecxsignlanguage.vercel.app/', // Your frontend URL
         methods: 'GET,POST,PUT,DELETE',
         allowedHeaders: 'Content-Type,Authorization',
     })
@@ -34,15 +33,9 @@ mongoose
     .catch((err) => console.log('MongoDB connection error: ', err));
 
 // Routes
-
 app.use('/api/auth', authRoutes.router);
 app.use('/api/paths', require('./routes/pathRoutes'));
 app.use('/api/lessons', require('./routes/lessonRoutes'));
 
-// Export app for testing purposes
-// module.exports = app;
-
-// // Start server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Export app as the handler for Vercel's serverless function
+module.exports = app;
