df74e4a4b021f8dee87ca0097aaf878748a280f2
Stadav01 <stanley.ndlovu@ashesi.edu.gh>
2025-01-07 14:53:48 +0000

    server

--- server.js
+++ server.js
@@ -5,37 +5,36 @@ const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(bodyParser.json());
 
 const cors = require('cors');
-
-// Enable CORS for all routes
 app.use(cors());
 
-// Or configure CORS to allow specific origin
-app.use(
-    cors({
-        origin: 'https://edutecxsignlanguage.vercel.app/', // Your frontend URL
-        methods: 'GET,POST,PUT,DELETE',
-        allowedHeaders: 'Content-Type,Authorization',
-    })
-);
-
 // Connect to MongoDB
-mongoose
-    .connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log('MongoDB connection error: ', err));
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('MongoDB connection error: ', err);
+        process.exit(1); // Exit the process if the connection fails
+    }
+};
+
+connectDB();
 
 // Routes
 app.use('/api/auth', authRoutes.router);
 app.use('/api/paths', require('./routes/pathRoutes'));
 app.use('/api/lessons', require('./routes/lessonRoutes'));
 
-// Export app as the handler for Vercel's serverless function
-module.exports = app;
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
